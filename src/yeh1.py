10404329a9de3661f586abbedb54e5a99a8760fd
yeh1 <yeh1@rose-hulman.edu>
2018-10-29 20:48:46 -0400

    1

--- src/yeh1.py
+++ src/yeh1.py
@@ -9,16 +9,18 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-    #   test_wait_until_pressed()
-    test_wait_until_released()
+    test_wait_until_pressed()
+    #test_wait_until_released()
 
 
 
 def test_wait_until_pressed():
     robot = rb.Snatch3rRobot()
-    robot.drive_system.start_moving(50, 50)
-    robot.touch_sensor.wait_until_pressed()
-    robot.drive_system.stop_moving()
+    while True:
+        robot.drive_system.start_moving(50, 50)
+        robot.touch_sensor.wait_until_pressed()
+        robot.drive_system.stop_moving()
+        robot.touch_sensor.wait_until_released()
 
 def test_wait_until_released():
     robot = rb.Snatch3rRobot()
